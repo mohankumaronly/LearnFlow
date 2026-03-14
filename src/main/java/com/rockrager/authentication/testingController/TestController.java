f2ded5a985247d7a7cf4c50cbf00a8f6ad8b2815
mohankumaronly <mohankumaronly81@gmail.com>
2026-03-14 11:40:48 +0530

    new java project is initialized here

---
+++ src/main/java/com/rockrager/authentication/testingController/TestController.java
@@ -0,0 +1,13 @@
+package com.rockrager.authentication.testingController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String testApi(){
+        return "Rockrangerz Spring Boot Docker Test Successful";
+    }
+
+}
